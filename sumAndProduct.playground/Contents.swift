23fa30475845ac43a1b8154802ab01ac2d8c6719
Ife Banire <Ifebanire@yahoo.com>
2019-09-26 22:09:12 -0400

    code completed

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,19 @@
 import UIKit
 
-func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
-    
+func sumAndProduct(_ sum: Int, _ product: Int) -> Array<Int> {
+    for x in 1...sum {
+        for y in 1...sum {
+            if x+y == sum && x*y == product {
+                if x > y {
+                    return [x,y]
+                } else {
+                    return [y,x]
+                }
+            }
+        }
+    }
+    return [Int]()
 }
+
+
+sumAndProduct(2, 3)
